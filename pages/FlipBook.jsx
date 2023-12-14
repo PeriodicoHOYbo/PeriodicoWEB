d0b3233ea0deb31a7dc1e70343e505b59bc5e055
raulDeDevWorld <proyection009@gmail.com>
2023-12-14 13:08:49 -0400

    DC

--- pages/FlipBook.jsx
+++ pages/FlipBook.jsx
@@ -11,17 +11,17 @@ function MyBook(props) {
     console.log(router.query.edicion)
 
     return (
-        userDB && userDB !== undefined && userDB.EdicionDigital && userDB.EdicionDigital[ router.query.edicion] && userDB.EdicionDigital[ router.query.edicion] !== undefined && <div className=' bg-[#00404a] h-screen w-full flex items-center overflow-hidden sm:h-auto sm:min-h-screen sm:pt-[75px]'>
+        userDB && userDB !== undefined && userDB.EdicionDigital && userDB.EdicionDigital[ router.query.edicion] && userDB.EdicionDigital[ router.query.edicion] !== undefined && <div className='relative top-[-200px] bg-[#00404a] h-screen w-full flex items-center overflow-hidden sm:h-auto sm:min-h-screen'>
             <NavbarBack></NavbarBack>
 
             <button className='fixed text-[20px] text-gray-500 h-[40px] w-[100px] text-white rounded-full inline-block left-[0px] top-0 bottom-0 my-auto bg-[#00000080] z-20 lg:left-[20px] pointer-events-none' >Anterior</button>
             <button className='fixed text-[20px] text-gray-500 h-[40px] w-[100px] text-white rounded-full inline-block right-[0px] top-0 bottom-0 my-auto bg-[#00000080] z-20 lg:right-[20px] pointer-events-none' >Siguiente</button>
 
-            <HTMLFlipBook width={window.screen.width - 200} height={window.screen.width * 1.3}
+            <HTMLFlipBook width={window.screen.width - 300} height={window.screen.width * 1.3}
 
                 maxShadowOpacity={0.5}
                 showCover={false}
-                swipeDistance={300}
+                swipeDistance={100}
                 flippingTime={500}
                 mobileScrollSupport={true}
             >
