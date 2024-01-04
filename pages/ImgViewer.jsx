32676b502e1d591f0902b99bd0e1dcd08e271a19
PeriodicoHOYbo <135343550+PeriodicoHOYbo@users.noreply.github.com>
2024-01-03 20:14:50 -0400

    Update ImgViewer.jsx
    DC

--- pages/ImgViewer.jsx
+++ pages/ImgViewer.jsx
@@ -72,8 +72,8 @@ function TemplateOne() {
     setFormViewer(!formViewer)
   }
 
-  function closeModal() {
-   
+  function closeModal(e) {
+   e.stopPropagation()
     let navegador = navigator.userAgent;
     if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
       setButtons(false)
