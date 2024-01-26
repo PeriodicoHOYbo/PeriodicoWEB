c80fb8f5e6b16e6984fd8e978aa6dbd2cc9e3d4c
PeriodicoHOYbo <135343550+PeriodicoHOYbo@users.noreply.github.com>
2024-01-26 13:25:26 -0400

    Update FlipBook.jsx
    DC

--- pages/FlipBook.jsx
+++ pages/FlipBook.jsx
@@ -17,7 +17,7 @@ function MyBook(props) {
             <button className='fixed text-[20px] text-gray-500 h-[40px] w-[100px] text-white rounded-full inline-block left-[0px] top-0 bottom-0 my-auto bg-[#00000080] z-20 lg:left-[20px] pointer-events-none' >Anterior</button>
             <button className='fixed text-[20px] text-gray-500 h-[40px] w-[100px] text-white rounded-full inline-block right-[0px] top-0 bottom-0 my-auto bg-[#00000080] z-20 lg:right-[20px] pointer-events-none' >Siguiente</button>
 
-            <HTMLFlipBook width={window.screen.width - 300} height={window.screen.width * 1.3}
+            <HTMLFlipBook width={window.screen.width - 300} height={window.screen.width * 1.2}
 
                 maxShadowOpacity={0.5}
                 showCover={false}
