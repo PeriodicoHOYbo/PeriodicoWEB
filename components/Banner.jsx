793b291e648fc1bf80b0a29f928cfed63d5d7f97
PeriodicoHOYbo <135343550+PeriodicoHOYbo@users.noreply.github.com>
2024-02-20 20:39:40 -0400

    Update Banner.jsx
    DC

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -44,7 +44,7 @@ export default function Banner({ ruta, carpeta, click }) {
                                             :
                                             <>
                                                 <button className={styles.editButton}>Edit</button>
-                                                {userDB[ruta][`${carpeta}`][i].enlace.includes('https://www.youtube')
+                                                { userDB[ruta][`${carpeta}`][i].enlace && userDB[ruta][`${carpeta}`][i].enlace.includes('https://www.youtube')
                                                     ? <iframe
                                                         className={`${styles.responsiveIframe} h-[200px] md:h-[300px]`}
                                                         src={userDB[ruta][`${carpeta}`][i].enlace.includes('https://www.youtube') ? userDB[ruta][`${carpeta}`][i].enlace.replace('/watch?v=', '/embed/') + '?showinfo=0' : userDB[ruta][`${carpeta}`][i].enlace}
