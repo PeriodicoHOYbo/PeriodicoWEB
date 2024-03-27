f1a033563caa529c3c61c70bb3115345fe80beda
PeriodicoHOYbo <135343550+PeriodicoHOYbo@users.noreply.github.com>
2024-03-26 22:24:32 -0400

    Update Layout.jsx

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -158,8 +158,7 @@ function Layout({ children }) {
 
                             </Link>
                             <Link href="https://swoou.com/" legacyBehavior scroll={false}>
-                                <a onClick={handlerClick}> <span>
-©TARKAN Ltda.</span></a>
+                                <a onClick={handlerClick}> <span>©TARKAN Ltda.</span></a>
                             </Link>
                         </div>
                     </footer>
