4117c324069a8a4f451bb5e40f574e44fb6ca205
PeriodicoHOYbo <135343550+PeriodicoHOYbo@users.noreply.github.com>
2024-04-10 21:46:28 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -52,7 +52,7 @@ export default function Navbar() {
             </div>
 
             <div className={style.container}>
-                <nav className={`${style.nav}`} style={{ padding: '0 80px', position: 'relative' }}>
+                <nav className={`${style.nav}`} style={{ padding: '0 100px', position: 'relative' }}>
                     <Link href="https://hoy.bo/" legacyBehavior scroll={false}>
                         <a className={` absolute pt-[5px] h-[30px] top-0 left-[0px] ${pathname == "#Sociedad" ? style.active : ''}`} onClick={handlerClick}>
                             <span className=' h-[30px] rounded-full flex justify-center items-center bg-white rounded'><img src={'/inicio_v2.jpeg'} className="block h-[30px] rounded-full cursor-pointer" onClick={() => router.push('/')} alt="" /></span>
